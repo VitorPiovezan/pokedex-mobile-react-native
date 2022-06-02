aa1a9081e748a32449e8fc9097d3802277b3f8b4
VitorPiovezan <vitor.piovezan@hotmail.com.br>
2022-06-01 22:36:29 -0300

    Create Icon Menu and TextImput Search in HomePage

--- src/screens/home/Home.style.tsx
+++ src/screens/home/Home.style.tsx
@@ -5,11 +5,38 @@ export const Container = styled.View`
   background-color: #090f0b;
   align-items: center;
 `;
+export const Header = styled.View`
+  width: 90%;
+  padding: 30px 0px;
+`;
 
 export const Title = styled.Text`
   color: #fff;
   font-size: 26px;
   font-weight: bold;
   width: 70%;
-  top: 80px;
+  padding-top: 30px;
+`;
+
+export const ImageMenu = styled.Image`
+  width: 25px;
+`;
+
+export const SearchHomeView = styled.View`
+  width: 90%;
+`;
+
+export const SearchHome = styled.TextInput`
+  color: #fff;
+  background-color: #3e4047;
+  padding: 10px 50px;
+  border-radius: 25px;
+`;
+
+export const IconSearch = styled.Image`
+  position: absolute;
+  width: 20px;
+  height: 20px;
+  margin: 13px 20px
+  z-index: 3;
 `;
--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,11 +1,29 @@
-import { Container, Title } from './Home.style';
-import { StatusBar, Text } from 'react-native';
+import {
+  Container,
+  Title,
+  Header,
+  ImageMenu,
+  SearchHome,
+  IconSearch,
+  SearchHomeView,
+} from './Home.style';
+import { StatusBar, View } from 'react-native';
 import React from 'react';
 
 export default function HomePage() {
   return (
     <Container>
-      <Title>Qual Pokémon você está procurando?</Title>
+      <Header>
+        <ImageMenu source={require('../../../assets/icons/menu.png')} />
+        <Title>Qual Pokémon você está procurando?</Title>
+      </Header>
+      <SearchHomeView>
+        <IconSearch source={require('../../../assets/icons/loupe_gray.png')} />
+        <SearchHome
+          placeholder="Buscar Pokémon"
+          placeholderTextColor="#b8b6b3"
+        />
+      </SearchHomeView>
       <StatusBar style="auto" />
     </Container>
   );
